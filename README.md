f95c9a50334bab45201e26924f96e8f908429f8c
hdkef <hdkef11@gmail.com>
2021-06-17 23:02:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Long Story
+long story is a youtube-clone web app that is made for educational purposes
+
+# Stack being used
+Angular
+Golang
+
+HLS protocol and shaka player is being heavily used in this web app
---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "longstory/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
